78abdc7f558e47abf70edf2bab07ea996987e7f8
tip_cototrigger <cototrigger@gmail.com>
2022-05-31 13:11:53 +0700

    Lession 2

--- les-2/02.py
+++ les-2/02.py
@@ -1,5 +1,4 @@
-list = input("Введите элементы списка через пробел: ").split()
+list = input("Введите элементы списка через пробел: ").split('')
 for i in range(0, len(list)-1, 2):
     list[i], list[i+1] = list[i+1], list[i]
-
 print(list)
